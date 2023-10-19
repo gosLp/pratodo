b24cbefbce1b2418df7b00a3bec938dba75b90d3
Pratheek Shetty <68729469+gosLp@users.noreply.github.com>
2023-10-19 17:53:21 +0530

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+    signin() {
+        return { msg: 'I am signed in'}
+    }
+
+    signup() {
+        return { msg: 'I am signed up'}
+    }
+}
+
+
+
