e2f04b62a4b40f3c357e0034e0b565443d4f28ec
Pratheek Shetty <pratheekshetty@Pratheeks-MacBook-Pro.local>
2023-10-19 21:33:15 +0530

    auth service added

---
+++ prisma/migrations/20231019130918_initial/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,26 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('signin')
-    signin() {
+    signin(@Body() dto: AuthDto) {
+        
+        console.log({
+            dto,
+        });
         return this.authService.signin();
     }
 
     @Post('signup')
-    signup() {
+    signup(@Body() dto: AuthDto) {
+
+        console.log({
+            dto,
+        })
         return this.authService.signup();
     }
 }
